147fa6432783651b8c97699fd4e8cdcf437e9e0c
Brian Power <powerbf.it@gmail.com>
2021-03-01 19:46:08 +1000

    i18n: Added monster message functions

--- crawl-ref/source/actor-util.h
+++ crawl-ref/source/actor-util.h
@@ -51,3 +51,15 @@ void do_actor_message(const actor* subject, bool subject_seen,
                       const string& other_msg,
                       const string& yourself_msg,
                       const string& itself_msg);
+
+
+string get_monster_message(const actor* subject, bool subject_seen,
+                           const actor* object, bool object_seen,
+                           const string& you_obj_msg,
+                           const string& other_msg);
+
+void do_monster_message(const actor* subject, bool subject_seen,
+                        const actor* object, bool object_seen,
+                        const string& you_obj_msg,
+                        const string& other_msg);
+
