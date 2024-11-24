9747a885b036469043cf6a91ff55a604e7a83a51
Brian Power <powerbf.it@gmail.com>
2024-11-24 22:19:03 +1000

    i18n: Reorganise and expand German monster tests

--- crawl-ref/source/test/i18n/de/mon-test.cc
+++ crawl-ref/source/test/i18n/de/mon-test.cc
@@ -55,110 +55,149 @@ int main()
     databaseSystemInit(true);
     init_localisation("de");
 
-    // with definite article
+    // simple masculine
     test("der Ork", "the orc");
-    test("die Königspython", "the ball python");
-    test("das Skelett", "the skeleton");
-    cout << endl;
-
-    // with indefinite article
     test("ein Ork", "an orc");
-    test("eine Königspython", "a ball python");
-    test("ein Skelett", "a skeleton");
-    cout << endl;
-
-    // with "your"
     test("dein Ork", "your orc");
-    test("deine Königspython", "your ball python");
-    test("dein Skelett", "your skeleton");
-    cout << endl;
-
-    // with no determiner
     test("Ork", "orc");
+    test("Du triffst den Ork", "You hit %s", "the orc");
+    test("Du kollidierst mit dem Ork!", "You collide with %s!", "the orc");
+    cout << endl;
+
+    // simple feminine
+    test("die Königspython", "the ball python");
+    test("eine Königspython", "a ball python");
+    test("deine Königspython", "your ball python");
     test("Königspython", "ball python");
+    test("Du triffst die Königspython", "You hit %s", "the ball python");
+    test("Du kollidierst mit der Königspython!", "You collide with %s!", "the ball python");
+    cout << endl;
+
+    // simple neuter
+    test("das Skelett", "the skeleton");
+    test("ein Skelett", "a skeleton");
+    test("dein Skelett", "your skeleton");
     test("Skelett", "skeleton");
+    test("Du triffst das Skelett", "You hit %s", "the skeleton");
+    test("Du kollidierst mit dem Skelett!", "You collide with %s!", "the skeleton");
     cout << endl;
 
-    // with definite article and adjective
+    // masculine with adjective
     test("der hilflose Ork", "the helpless orc");
-    test("die hilflose Königspython", "the helpless ball python");
-    test("das hilflose Skelett", "the helpless skeleton");
-    cout << endl;
-
-    // with indefinite article and adjective
     test("ein hilfloser Ork", "a helpless orc");
-    test("eine hilflose Königspython", "a helpless ball python");
-    test("ein hilfloses Skelett", "a helpless skeleton");
-    cout << endl;
-
-    // with "your" and adjective
     test("dein hilfloser Ork", "your helpless orc");
-    test("deine hilflose Königspython", "your helpless ball python");
-    test("dein hilfloses Skelett", "your helpless skeleton");
-    cout << endl;
-
-    // with no determiner and adjective
     test("hilfloser Ork", "helpless orc");
+    test("Du triffst den hilflosen Ork", "You hit %s", "the helpless orc");
+    test("Du kollidierst mit dem hilflosen Ork!", "You collide with %s!", "the helpless orc");
+    cout << endl;
+
+    // feminine with adjective
+    test("die hilflose Königspython", "the helpless ball python");
+    test("eine hilflose Königspython", "a helpless ball python");
+    test("deine hilflose Königspython", "your helpless ball python");
     test("hilflose Königspython", "helpless ball python");
+    test("Du triffst die hilflose Königspython", "You hit %s", "the helpless ball python");
+    test("Du kollidierst mit der hilflosen Königspython!", "You collide with %s!", "the helpless ball python");
+    cout << endl;
+
+    // neuter with adjective
+    test("das hilflose Skelett", "the helpless skeleton");
+    test("ein hilfloses Skelett", "a helpless skeleton");
+    test("dein hilfloses Skelett", "your helpless skeleton");
     test("hilfloses Skelett", "helpless skeleton");
+    test("Du triffst das hilflose Skelett", "You hit %s", "the helpless skeleton");
+    test("Du kollidierst mit dem hilflosen Skelett!", "You collide with %s!", "the helpless skeleton");
     cout << endl;
 
-    // uniques
-    test("Prinz Ribbit", "Prince Ribbit");
-    test("Verrückter Yiuf", "Crazy Yiuf");
+    // masculine with weak declension
+    test("der Feuerdrache", "the fire dragon");
+    test("ein Feuerdrache", "a fire dragon");
+    test("dein Feuerdrache", "your fire dragon");
+    test("Feuerdrache", "fire dragon");
+    test("der hilflose Feuerdrache", "the helpless fire dragon");
+    test("ein hilfloser Feuerdrache", "a helpless fire dragon");
+    test("dein hilfloser Feuerdrache", "your helpless fire dragon");
+    test("hilfloser Feuerdrache", "helpless fire dragon");
+    test("Du triffst den Feuerdrachen", "You hit %s", "the fire dragon");
+    test("Du triffst den hilflosen Feuerdrachen", "You hit %s", "the helpless fire dragon");
+    test("Du kollidierst mit dem Feuerdrachen!", "You collide with %s!", "the fire dragon");
+    test("Du kollidierst mit dem hilflosen Feuerdrachen!", "You collide with %s!", "the helpless fire dragon");
+    cout << endl;
+
+    // unique with simple name
     test("Natascha", "Natasha");
+    test("Du triffst Natascha", "You hit %s", "Natasha");
+    test("Du triffst die hilflose Natascha", "You hit %s", "the helpless Natasha");
+    test("Du kollidierst mit Natascha!", "You collide with %s!", "Natascha");
+    test("Du kollidierst mit der hilflosen Natascha!", "You collide with %s!", "the helpless Natasha");
     cout << endl;
 
-    // uniques with definite article
-    test("Blork der Ork", "Blork the orc");
+    // unique with definite article in name
     test("die Zauberin", "the Enchantress");
-    test("die Lernäische Hydra", "the Lernaean hydra");
-    test("die Höllenschlange", "the Serpent of Hell");
-    test("das Gelée Royale", "the Royal Jelly");
+    test("Zauberin", "Enchantress");
+    test("Du triffst die Zauberin", "You hit %s", "the Enchantress");
+    test("Du triffst die hilflose Zauberin", "You hit %s", "the helpless Enchantress");
+    test("Du kollidierst mit der Zauberin!", "You collide with %s!", "the Enchantress");
+    test("Du kollidierst mit der hilflosen Zauberin!", "You collide with %s!", "the helpless Enchantress");
     cout << endl;
 
-    // uniques with definite article and adjective
+    // unique with weak declension
+    test("Prinz Ribbit", "Prince Ribbit");
     test("der hilflose Prinz Ribbit", "the helpless Prince Ribbit");
-    test("der hilflose Verrückte Yiuf", "the helpless Crazy Yiuf");
-    //test("der hilflose Blork der Ork", "the helpless Blork the orc");
-    test("die hilflose Zauberin", "the helpless Enchantress");
-    test("die hilflose 27-köpfige Lernäische Hydra", "the helpless 27-headed Lernaean hydra");
-    test("die hilflose Höllenschlange", "the helpless Serpent of Hell");
-    test("das hilflose Gelée Royale", "the helpless Royal Jelly");
-    cout << endl;
-
-    // uniques with adjective only
     test("hilfloser Prinz Ribbit", "helpless Prince Ribbit");
-    test("hilfloser Verrückter Yiuf", "helpless Crazy Yiuf");
-    //test("hilfloser Blork der Ork", "helpless Blork the orc");
-    test("hilflose Zauberin", "helpless Enchantress");
-    test("hilflose 27-köpfige Lernäische Hydra", "helpless 27-headed Lernaean hydra");
-    test("hilflose Höllenschlange", "helpless Serpent of Hell");
-    test("hilfloses Gelée Royale", "helpless Royal Jelly");
+    test("Du triffst Prinzen Ribbit", "You hit %s", "Prince Ribbit");
+    test("Du triffst den hilflosen Prinzen Ribbit", "You hit %s", "the helpless Prince Ribbit");
+    test("Du kollidierst mit Prinzen Ribbit!", "You collide with %s!", "Prince Ribbit");
+    test("Du kollidierst mit dem hilflosen Prinzen Ribbit!", "You collide with %s!", "the helpless Prince Ribbit");
     cout << endl;
 
-    // special cases
-    test("der hilflose gelbe Drakonier", "the helpless yellow draconian");
+    // unique with a capitalised adjective in the name
+    test("Verrückter Yiuf", "Crazy Yiuf");
+    test("der hilflose Verrückte Yiuf", "the helpless Crazy Yiuf");
+    test("hilfloser Verrückter Yiuf", "helpless Crazy Yiuf");
+    test("Du triffst Verrückten Yiuf", "You hit %s", "Crazy Yiuf");
+    test("Du triffst den hilflosen Verrückten Yiuf", "You hit %s", "the helpless Crazy Yiuf");
+    test("Du kollidierst mit Verrücktem Yiuf!", "You collide with %s!", "Crazy Yiuf");
+    test("Du kollidierst mit dem hilflosen Verrückten Yiuf!", "You collide with %s!", "the helpless Crazy Yiuf");
+    cout << endl;
+
+    // another unique with a capitalised adjective in the name
+    test("die Lernäische Hydra", "the Lernaean hydra");
+    test("die hilflose Lernäische Hydra", "the helpless Lernaean hydra");
+    test("hilflose Lernäische Hydra", "helpless Lernaean hydra");
+    test("die 27-köpfige Lernäische Hydra", "the 27-headed Lernaean hydra");
+    test("die hilflose 27-köpfige Lernäische Hydra", "the helpless 27-headed Lernaean hydra");
+    test("Du triffst die hilflose 27-köpfige Lernäische Hydra", "You hit %s", "the helpless 27-headed Lernaean hydra");
+    test("Du kollidierst mit der hilflosen 27-köpfigen Lernäischen Hydra!", "You collide with %s!", "the helpless 27-headed Lernaean hydra");
+    cout << endl;
+
+    // unique with "of" in the English name (this could mess things up)
+    test("die Höllenschlange", "the Serpent of Hell");
+    test("Höllenschlange", "Serpent of Hell");
+    test("die hilflose Höllenschlange", "the helpless Serpent of Hell");
+    test("Du triffst die Höllenschlange", "You hit %s", "the Serpent of Hell");
+    test("Du triffst die hilflose Höllenschlange", "You hit %s", "the helpless Serpent of Hell");
+    test("Du kollidierst mit der Höllenschlange!", "You collide with %s!", "the Serpent of Hell");
+    test("Du kollidierst mit der hilflosen Höllenschlange!", "You collide with %s!", "the helpless Serpent of Hell");
+    cout << endl;
+
+    // unique with definite article in the middle
+    test("Blork der Ork", "Blork the orc");
+    // these ones currently fail
+    //test("der hilflose Blork der Ork", "the helpless Blork the orc");
+    //test("hilfloser Blork der Ork", "helpless Blork the orc");
+    test("Du triffst Blork den Ork", "You hit %s", "Blork the orc");
+    test("Du kollidierst mit Blork dem Ork!", "You collide with %s!", "Blork the orc");
+    cout << endl;
+
+    // named ally
     test("Boghold der Ork-Warlord", "Boghold the orc warlord");
     //test("der hilflose Boghold der Ork-Warlord", "the helpless Boghold the orc warlord");
     cout << endl;
 
-    // accusative case
-    test("Du triffst den Ork", "You hit %s", "the orc");
-    test("Du triffst die Königspython", "You hit %s", "the ball python");
-    test("Du triffst das Skelett", "You hit %s", "the skeleton");
-    test("Du triffst den hilflosen Ork", "You hit %s", "the helpless orc");
-    test("Du triffst die 27-köpfige Lernäische Hydra", "You hit %s", "the 27-headed Lernaean hydra");
-    test("Du triffst den hilflosen Verrückten Yiuf", "You hit %s", "the helpless Crazy Yiuf");
-    test("Du triffst den hilflosen gelben Drakonier", "You hit %s", "the helpless yellow draconian");
-    cout << endl;
-
-    // dative case
-    test("Du kollidierst mit dem Ork!", "You collide with %s!", "the orc");
-    test("Du kollidierst mit der Königspython!", "You collide with %s!", "the ball python");
-    test("Du kollidierst mit dem Skelett!", "You collide with %s!", "the skeleton");
-    test("Du kollidierst mit der 27-köpfigen Lernäischen Hydra!", "You collide with %s!", "the 27-headed Lernaean hydra");
-    cout << endl;
+    // derived monsters (should have the gender of the derived monster, not the original)
+    test("Du triffst den hilflosen Meervolk-Zombie", "You hit %s", "the helpless merfolk zombie");
+    test("Du triffst das hilflose Ork-Skelett", "You hit %s", "the helpless orc skeleton");
 
     // show results summary
     cout << num_passes << " TESTS PASSED" << endl;
