dbf90831dd06187d9e3978903b96741edaf25120
Brian Power <powerbf.it@gmail.com>
2025-02-13 16:08:18 +1000

    i18n: Improve parsing of args to mon-speech-test

--- crawl-ref/source/test/i18n/mon-speech-test.cc
+++ crawl-ref/source/test/i18n/mon-speech-test.cc
@@ -14,24 +14,40 @@
 #include <map>
 using namespace std;
 
+static void _show_usage()
+{
+    cerr << "Usage: mon-speech-test <language> <string key> <iterations> [<RNG seed>]"
+         << endl;
+}
 
 int main(int argc, char** argv)
 {
     if (argc < 4 || argc > 5)
     {
-        cerr << "Usage: mon-speech-test <language> <string key> <iterations> [<RNG seed>]"
-             << endl;
+        _show_usage();
         return 1;
     }
 
     string lang = argv[1];
     string key = argv[2];
-    unsigned iterations = (unsigned)strtoul(argv[3], nullptr, 10);
+
+    int iterations = 0;
+    if (!parse_int(argv[3], iterations) || iterations < 1)
+    {
+        _show_usage();
+        return 1;
+    }
 
     if (argc > 4)
     {
         // seed RNG with specified seed value
-        uint64_t seed = strtoul(argv[4], nullptr, 10);
+        uint64_t seed = 0;
+        if (sscanf(argv[4], "%" SCNu64, &seed) < 1)
+        {
+            _show_usage();
+            return 1;
+        }
+        //cout << "Using RNG seed: " << seed << endl;
         rng::seed(seed);
     }
     else
@@ -58,7 +74,7 @@ int main(int argc, char** argv)
     dummy->hit_points = 20;
     dummy->position = coord_def(10, 9);
 
-    for (unsigned i = 0; i < iterations; i++)
+    for (int i = 0; i < iterations; i++)
     {
         string msg = getSpeakString(key);
         msg = do_mon_str_replacements(msg, *dummy);
