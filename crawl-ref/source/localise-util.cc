e172caa7e3574fadc70702c9ec265662b96f845f
Brian Power <powerbf.it@gmail.com>
2026-08-15 19:36:40 +1000

    i18n: Implement pattern matching

--- crawl-ref/source/localise-util.cc
+++ crawl-ref/source/localise-util.cc
@@ -25,10 +25,42 @@ string cxlate(const string &context, const string &s)
     string result;
 
     // first try with context
-    result = getTranslatedString(add_context_to_string(context, s));
-    if (!result.empty())
-        return result;
+    if (!context.empty()) {
+        result = getTranslatedString(add_context_to_string(context, s));
+        if (!result.empty())
+            return result;
+    }
 
-    // now try without
+    // fall back to the default translation
     return getTranslatedString(s);
+}
+
+// extract @foo@ parameters from string
+vector<string> extract_params(const string& s)
+{
+    vector<string> results;
+
+    size_t start = s.find('@', 0);
+    while (start != string::npos) {
+        size_t end = s.find('@', start + 1);
+        if (end == string::npos)
+            break;
+        results.push_back(s.substr(start, end - start + 1));
+        start = s.find('@', end + 1);
+    }
+
+    return results;
+}
+
+string escape_regex_specials(const string& s)
+{
+    const string specials = "\\^$.|?*+()[]{}";
+    string result;
+    for (char c: s)
+    {
+        if (specials.find(c) != string::npos)
+            result += '\\';
+        result += c;
+    }
+    return result;
 }
--- crawl-ref/source/localise-util.h
+++ crawl-ref/source/localise-util.h
@@ -6,8 +6,10 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 using std::string;
+using std::vector;
 
 
 // low-level translate function
@@ -17,3 +19,8 @@ string xlate(const string& s);
 string cxlate(const string& context, const string &s);
 
 string add_context_to_string(const string &context, const string &s);
+
+vector<string> extract_params(const string& s);
+
+// escape any characters that have a special meaning in regex
+string escape_regex_specials(const string& s);
