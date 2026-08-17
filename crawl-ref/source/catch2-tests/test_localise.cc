842d4ec231e2cdab4abb60ee53a6efaecf0f22c5
Brian Power <powerbf.it@gmail.com>
2026-08-17 21:25:23 +1000

    i18n: Add catch2 test for localise

---
+++ crawl-ref/source/catch2-tests/test_localise.cc
@@ -0,0 +1,25 @@
+#include "catch_amalgamated.hpp"
+
+#include "AppHdr.h"
+#include "database.h"
+#include "initfile.h"
+#include "localise.h"
+#include "options.h"
+
+#include <unistd.h>
+
+TEST_CASE( "Localise", "[single-file]" )
+{
+    setlocale(LC_ALL, "");
+    Options.lang_name = "de";
+    Options.language = lang_t::DE;
+    SysEnv.crawl_dir = ".";
+    databaseSystemInit();
+    init_localisation();
+
+    SECTION("Sentence")
+    {
+        CHECK( localise("You kill the rat!") == "Du tötest die Ratte!" );
+        //CHECK( localise("You kill the orc!") == "Du tötest den Ork!" );
+    }
+}
