35c61a0b89519024be8f845a0e8e02222dfcb8d0
Brian Power <powerbf.it@gmail.com>
2026-08-17 23:43:23 +1000

    i18n: Update test to use a message that still exists in the code

--- crawl-ref/source/catch2-tests/test_localise.cc
+++ crawl-ref/source/catch2-tests/test_localise.cc
@@ -21,7 +21,7 @@ TEST_CASE( "Localise", "[single-file]" )
     {
         CHECK( localise("You kill the rat!") == "Du tötest die Ratte!" );
         CHECK( localise("You kill the orc!") == "Du tötest den Ork!" );
-        CHECK( localise("The orc shivers with cold") == "Der Ork zittert vor Kälte" );
+        CHECK( localise("The broad axe dances into the air!") == "Die Breitaxt tanzt in die Luft!" );
     }
 
     databaseSystemShutdown();
