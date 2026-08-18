a00f7704722508335c1fabc05db8796daeeaa345
Brian Power <powerbf.it@gmail.com>
2026-08-18 20:47:52 +1000

    i18n: Pregenerate derived forms

--- crawl-ref/source/catch2-tests/test_localise.cc
+++ crawl-ref/source/catch2-tests/test_localise.cc
@@ -22,6 +22,7 @@ TEST_CASE( "Localise", "[single-file]" )
         CHECK( localise("You kill the rat!") == "Du tötest die Ratte!" );
         CHECK( localise("You kill the orc!") == "Du tötest den Ork!" );
         CHECK( localise("The broad axe dances into the air!") == "Die Breitaxt tanzt in die Luft!" );
+        CHECK( localise("The gnoll's halberd dances into the air!") == "Die Hellebarde des Gnolls tanzt in die Luft!" );
     }
 
     databaseSystemShutdown();
