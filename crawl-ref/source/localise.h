afccb2e3327ddfacfe9a876e0833c326ae39c77a
Brian Power <powerbf.it@gmail.com>
2026-08-22 15:34:26 +1000

    i18n: Handle different plural forms for different numbers

--- crawl-ref/source/localise.h
+++ crawl-ref/source/localise.h
@@ -11,6 +11,9 @@ using std::string;
 
 void init_localisation();
 
+// not mandatory to call this, except in regression tests, where we want to switch languages
+void shutdown_localisation();
+
 // Is localisation active?
 bool localisation_active();
 
