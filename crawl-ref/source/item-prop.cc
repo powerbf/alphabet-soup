323b7999ee392865cb0d35e8b21b7fbf6070ef96
Brian Power <powerbf.it@gmail.com>
2026-08-23 22:23:54 +1000

    i18n: Update some item strings

--- crawl-ref/source/item-prop.cc
+++ crawl-ref/source/item-prop.cc
@@ -3483,8 +3483,7 @@ static string _xp_evoker_recharge_msg(const item_def &evoker, int gained, bool s
         return msg;
     if (edata->max_charges == 1)
         return "%s has recharged.";
-    return make_stringf("%%s has regained %s charge%s.",
-                        number_in_words(gained).c_str(),
+    return make_stringf("%%s has regained %d charge%s.", gained,
                         gained > 1 ? "s" : "");
 }
 
