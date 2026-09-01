471b545b740fa8663e40f89ec09de6f68a19efcf
powerbf <45711639+powerbf@users.noreply.github.com>
2026-09-01 20:12:26 +1000

    i18n: Several fixes

--- crawl-ref/source/catch2-tests/test_localise.cc
+++ crawl-ref/source/catch2-tests/test_localise.cc
@@ -115,9 +115,9 @@ TEST_CASE( "Localise German", "[single-file]" )
         CHECK( localise("the merfolk zombie") == "der Zombie eines Meerwesens" );
         CHECK( localise("an orc skeleton") == "ein Skelett eines Orks" );
         CHECK( localise("a red draconian simulacrum") == "ein Simulacrum eines roten Drakoniers" );
-        /*CHECK( localise("a neutral orc skeleton") == "ein neutrales Skelett eines Orks" );
+        CHECK( localise("a neutral orc skeleton") == "ein neutrales Skelett eines Orks" );
         CHECK( localise("a non-hostile red draconian simulacrum") ==
-                        "ein nicht feindliches Simulacrum eines roten Drakoniers" );*/
+                        "ein nicht feindliches Simulacrum eines roten Drakoniers" );
     }
 
     SECTION("Items")
@@ -126,6 +126,13 @@ TEST_CASE( "Localise German", "[single-file]" )
         CHECK( localise("m - 2 potions of might") == "m - 2 Tränke der Macht" );
         CHECK( localise("a +2 leather armour" ) == "eine +2 Lederrüstung" );
         CHECK( localise("a +2 leather armour of fire resistance" ) == "eine +2 Lederrüstung der Feuerresistenz" );
+        CHECK( localise("a +1 pair of gloves of archery" ) == "ein +1 Paar Handschuhe der Schießkunst" );
+        CHECK( localise("the +7 war axe \"Hellblazer\" {flame rF+ Str+3}") ==
+                        "die +7 Kriegsaxt \"Hellblazer\" {Flammen rF+ Stä+3}" );
+        CHECK( localise("the +11 morningstar of Chance {antimagic, rElec rC- SInv}") ==
+                        "der +11 Morgenstern von Chance {Antimagie, rElek rK- UnsS}" );
+        CHECK( localise("the +7 hand cannon \"Mule\" {flame, backblast, rF+}") ==
+                        "die +7 Handkanone \"Mule\" {Flammen, Rückprall, rF+}" );
 
         // search result
         CHECK( localise("[Vaults:3] a +3 great sword of distortion (1 further duplicate)") ==
